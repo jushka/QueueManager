27c78261d058c2f72a6a5e7a200f32c10509c180
jushka <tautrimas25@gmail.com>
2019-09-18 00:19:44 +0300

    add functionality to show all clients in index.html

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const specialistsDiv = document.getElementById("specialists");
+
+function loadSpecialistClients(specialistName, specialistClients) {
+  let specialistDiv = document.createElement("div");
+  let specialistTitle = document.createElement("h2");
+  specialistTitle.textContent = specialistName;
+  specialistDiv.appendChild(specialistTitle);
+  specialistClients.map(client => {
+    let clientDiv = document.createElement("div");
+    clientDiv.className = "client";
+    let clientNumberDiv = document.createElement("div");
+    clientNumberDiv.className = "client__number";
+    let clientNameDiv = document.createElement("div");
+    clientNameDiv.className = "client__name";
+    let clientNumberPara = document.createElement("p");
+    clientNumberPara.textContent = client.number;
+    let clientNamePara = document.createElement("p");
+    clientNamePara.textContent = client.name;
+    clientNumberDiv.appendChild(clientNumberPara);
+    clientNameDiv.appendChild(clientNamePara);
+    clientDiv.appendChild(clientNumberDiv);
+    clientDiv.appendChild(clientNameDiv);
+    specialistDiv.appendChild(clientDiv);
+  });
+  specialistsDiv.appendChild(specialistDiv);
+}
+
+function loadData() {
+  let data = JSON.parse(window.localStorage.getItem("clients"));
+  data.map(specialist => {
+    loadSpecialistClients(specialist.specialist, specialist.clients);
+  });
+}
+
+if(window.localStorage.getItem("clients")) {
+  loadData();
+}
